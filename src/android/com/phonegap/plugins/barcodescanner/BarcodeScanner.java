16f72140f914fa8f3ef05abccde9a2e7d8a185bc
Soul Dmitry <onlygsmart@gmail.com>
2017-09-12 13:15:16 +0300

    test4

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -20,6 +20,7 @@ import com.google.zxing.client.android.Intents;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
