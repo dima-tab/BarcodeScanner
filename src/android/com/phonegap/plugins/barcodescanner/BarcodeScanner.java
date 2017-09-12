cb95ee41dd41a46ddbdaebc6f99cf2dcad41b297
Soul Dmitry <onlygsmart@gmail.com>
2017-09-12 16:54:00 +0300

    test16

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -142,7 +142,7 @@ public class BarcodeScanner extends CordovaPlugin {
         Point displaySize = getDisplaySize(activity);
 
         // set default size for scanning area
-        intentScan.putExtra(Intents.Scan.WIDTH, calculateEdge(0.5, displaySize.y));
+        intentScan.putExtra(Intents.Scan.WIDTH, calculateEdge(1.0, displaySize.y));
         intentScan.putExtra(Intents.Scan.HEIGHT, calculateEdge(0.5, displaySize.x));
 
         // add config as intent extras
